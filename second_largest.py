d4fe552a8f081a426dd30f8d99355fe5339af4c4
Ankur Manikandan <ankurmanikandan@Ankurs-iMac.attlocal.net>
2020-07-14 17:44:24 -0700

    determine second largest elements

---
+++ second_largest.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def second_larges_nbr(arr):
+    """Function to determine the second largest 
+    number given an unordered array."""
+
+    n = len(arr)
+
+    m1 = 0
+    m2 = 0
+
+    for i in range(n):
+        if m1 < arr[i]:
+            m1 = arr[i]
+            m2 = m1
+        if m1 < m2:
+            m1 = m2
+        
+    print("Second largest number in array: ", m2)
+
+if __name__ == '__main__':
+
+    # Randomly generate data
+    n = 50  # number of samples to generate
+    arr = list(range(n))
+    arr = np.random.choice(arr, replace=False, size=n)
+
+
+"""SQL Query: Determine the employee with the second largest salary.
+SELECT id, 
+FROM (SELECT id, RANK() OVER(ORDER BY salary DESC) AS rank_num 
+      FROM employee)
+where rank_num = 2;
+""" 
+
